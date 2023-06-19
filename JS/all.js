0b2ca945dfa8151f383f9b853ff432b40ed55d19
alanchou0305 <alan@hububble.co>
2023-06-19 18:16:09 +0800

    0619 v10 加Dropdown、Faq修理

--- JS/all.js
+++ JS/all.js
@@ -1,28 +1,58 @@
 $(document).ready(function () {
-  
-  // 點擊.filter-sign按钮时切换.clicked
-  $(".filter-sign").click(function() {
-    $(this).toggleClass("clicked");
+
+  // Filter
+
+  // 點擊.filter-selector-button按钮时切换.show
+  $(".filter-selector-button").click(function() {
+    $(".filter-selector-dropdown-menu").toggleClass("show");
   });
-  // 點擊其他地方移除.clicked
+
+  // 點擊其他地方移除.show
+  $(document).click(function(event) {
+    var target = $(event.target);
+    if (!target.closest('.filter-selector-button').length && !target.closest('.filter-selector-dropdown-menu').length) {
+      $('.filter-selector-dropdown-menu').removeClass('show');
+    }
+  });
+
+  $('.new-to-old').click(function(e) {
+    e.preventDefault();
+    $('.filter-selector-dropdown-menu').toggleClass('show');
+    $('.filter-selector-button-text').text('由新到舊');
+  });
+  
+  $('.old-to-new').click(function(e) {
+    e.preventDefault();
+    $('.filter-selector-dropdown-menu').toggleClass('show');
+    $('.filter-selector-button-text').text('由舊到新');
+  });
+  
+  // FAQ區塊
+  
+  // 點擊faq-item 加.clicked icon变回"add"
+  $(".faq-item").click(function(event) {
+    event.stopPropagation();
+  
+    if ($(this).hasClass("clicked")) {
+      $(this).removeClass("clicked");
+      $(this).find(".material-icons").text("add");
+    } else {
+      $(".faq-item.clicked").removeClass("clicked");
+      $(".faq-item .material-icons").text("add");
+  
+      $(this).addClass("clicked");
+      $(this).find(".material-icons").text("remove");
+    }
+  });
+  
+  // 點擊其他地方移除.clicked icon变回"add"
   $(document).click(function(event) {
     var clickedElement = event.target;
-    if (!$(clickedElement).hasClass("filter-sign")) {
-      $(".filter-sign").removeClass("clicked");
+  
+    if (!$(clickedElement).hasClass("faq-item")) {
+      $(".faq-item.clicked").removeClass("clicked");
+      $(".faq-item .material-icons").text("add");
     }
-  }); 
-
-  // 點擊.faq-item按钮时切换.clicked
-  $(".faq-item").click(function() {
-    $(this).toggleClass("clicked");
-  });
-  // 點擊其他地方移除.clicked
-  $(document).click(function(event) {    
-    if (!$(clickedElement).hasClass(".faq-item")) {
-      $(".faq-item").removeClass("clicked");
-    }
-  }); 
-
 
 // backtotop
 $(".backtotop").click(function (e) {
@@ -32,8 +62,7 @@ $(".backtotop").click(function (e) {
       scrollTop: 0,
     },
     1000
-  );
-});
-
-
+    );
+  });
+  });
 });
