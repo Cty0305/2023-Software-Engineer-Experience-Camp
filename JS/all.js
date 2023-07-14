0ef8cec5a0346d484af5124b49ccb654fb8557bc
alanchou0305 <alan@hububble.co>
2023-07-14 11:30:53 +0800

    0714 v19.3 update JSON

--- JS/all.js
+++ JS/all.js
@@ -149,7 +149,7 @@ $(document).ready(function () {
 
 // 資料串接
 const apiPath =
-  "https://raw.githubusercontent.com/alanchou0305/AI-Tool-King/65343a200adef60ec960ee2c8038546aa739fe3b/tools.json";
+  "https://raw.githubusercontent.com/alanchou0305/AI-Tool-King/main/tools.json";
 let worksData = [];
 let pagesData = [];
 
