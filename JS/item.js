37a93feec52c2818d1af3c87c295a8d904fd2c36
Cty0305 <mickeychou375@gmail.com>
2024-06-30 21:30:53 +0800

    0630 update filter, pagination

--- JS/item.js
+++ JS/item.js
@@ -1,11 +1,23 @@
 export function item() {
+  function scrollToTop() {
+    document
+      .querySelector(".container-mid")
+      .scrollIntoView({ behavior: "smooth" });
+  }
+
   const apiPath =
     "https://raw.githubusercontent.com/Cty0305/2023-Software-Engineer-Experience-Camp/main/tools.json";
   const cardList = document.querySelector(".card-list");
   const pagination = document.querySelector(".pagination");
 
   let worksData = [];
-  let pagesData = {};
+  let pagesData = {
+    total_items: 0,
+    total_pages: 0,
+    current_page: 1,
+    has_pre: false,
+    has_next: false,
+  };
 
   let data = {
     type: "",
@@ -26,18 +38,21 @@ export function item() {
           ...res.data.ai_works_page_3.data,
         ];
 
+        // 根据选定的类型进行筛选
+        if (type !== "") {
+          worksData = worksData.filter((item) => item.type === type);
+        }
+
         // 設置分頁信息
         const itemsPerPage = 6; // 每頁顯示的項目數
         const totalItems = worksData.length;
         const totalPages = Math.ceil(totalItems / itemsPerPage);
 
-        pagesData = {
-          total_items: totalItems,
-          total_pages: totalPages,
-          current_page: page,
-          has_pre: page > 1,
-          has_next: page < totalPages,
-        };
+        pagesData.total_items = totalItems;
+        pagesData.total_pages = totalPages;
+        pagesData.current_page = page;
+        pagesData.has_pre = page > 1;
+        pagesData.has_next = page < totalPages;
 
         // 根据 sort 进行排序
         worksData.sort((a, b) => {
@@ -46,14 +61,13 @@ export function item() {
           return sort === 1 ? dateB - dateA : dateA - dateB;
         });
 
-        // 使用 slice 方法分頁顯示數據
+        // 使用 slice 方法分页显示数据
         const startIndex = (page - 1) * itemsPerPage;
         const endIndex = startIndex + itemsPerPage;
         const paginatedData = worksData.slice(startIndex, endIndex);
 
-        console.log(worksData);
         renderWorks(paginatedData);
-        renderPages(pagesData);
+        renderPages();
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
@@ -69,36 +83,28 @@ export function item() {
   function renderWorks(paginatedData) {
     let works = "";
     paginatedData.forEach((item) => {
-      if (data.type === "" || item.type === data.type) {
-        works += /*html*/ `
-      <li class="card">
-        <div class="card-img">
-          <img src="${item.imageUrl}" alt="${item.title}">
-        </div>
-        <div class="card-content">
-          <div class="card-detail">
-            <div class="fs-h6">${item.title}</div>
-            <p class="fs-md">${item.description}</p>
+      works += /*html*/ `
+        <li class="card">
+          <div class="card-img">
+            <img src="${item.imageUrl}" alt="${item.title}">
           </div>
-          <div class="card-info">
-            <div class="fs-lg">AI 模型</div>
-            <div class="author fs-lg">${item.discordId}</div>
+          <div class="card-content">
+            <div class="card-detail">
+              <div class="fs-h6">${item.title}</div>
+              <p class="fs-md">${item.description}</p>
+            </div>
+            <div class="card-info">
+              <div class="fs-lg">AI 模型</div>
+              <div class="author fs-lg">${item.discordId}</div>
+            </div>
+            <div class="card-share">
+              <a href="#" class="item-type" data-type="${item.type}"><div class="fs-lg">${item.type}</div></a>
+              <a href="${item.link}" target="_blank"><span class="material-icons">open_in_new</span></a>
+            </div>
           </div>
-          <div class="card-share">
-            <a href=""><div class="fs-lg">${item.type}</div></a>
-            <a href="${item.link}"><span class="material-icons">open_in_new</span></a>
-          </div>
-        </div>
-      </li>`;
-      }
+        </li>`;
     });
     cardList.innerHTML = works;
-  }
-
-  function scrollToTop() {
-    document
-      .querySelector(".container-mid")
-      .scrollIntoView({ behavior: "smooth" });
   }
 
   // 绑定分页事件
@@ -110,11 +116,11 @@ export function item() {
       if (target.classList.contains("pagination-item")) {
         data.page = Number(target.dataset.page);
       } else if (target.classList.contains("prePage")) {
-        if (data.page > 1) {
+        if (pagesData.has_pre) {
           data.page -= 1;
         }
       } else if (target.classList.contains("nextPage")) {
-        if (data.page < pagesData.total_pages) {
+        if (pagesData.has_next) {
           data.page += 1;
         }
       } else {
@@ -127,34 +133,34 @@ export function item() {
   }
 
   // 渲染分页
-  function renderPages(pagesData) {
+  function renderPages() {
     let pageStr = "";
 
     pageStr += `
-  <li>
-    <a href="#" class="prePage material-icons ${
-      pagesData.has_pre ? "" : "disabled"
-    }">
-      keyboard_arrow_left
-    </a>
-  </li>`;
+      <li>
+        <a href="#" class="prePage material-icons ${
+          pagesData.has_pre ? "" : "disabled"
+        }">
+          keyboard_arrow_left
+        </a>
+      </li>`;
 
     for (let i = 1; i <= pagesData.total_pages; i += 1) {
       pageStr += `<li>
-      <a href="#" class="pagination-item fs-md ${
-        pagesData.current_page == i ? "active" : ""
-      }" data-page="${i}">${i}</a>
-    </li>`;
+        <a href="#" class="pagination-item fs-md ${
+          pagesData.current_page === i ? "active" : ""
+        }" data-page="${i}">${i}</a>
+      </li>`;
     }
 
     pageStr += `
-  <li>
-    <a href="#" class="nextPage material-icons ${
-      pagesData.has_next ? "" : "disabled"
-    }">
-      keyboard_arrow_right
-    </a>
-  </li>`;
+      <li>
+        <a href="#" class="nextPage material-icons ${
+          pagesData.has_next ? "" : "disabled"
+        }">
+          keyboard_arrow_right
+        </a>
+      </li>`;
 
     pagination.innerHTML = pageStr;
 
@@ -186,8 +192,8 @@ export function item() {
 
       data.page = 1; // 切换分类时重置到第一页
 
-      scrollToTop();
       getData(data);
+      scrollToTop();
     });
   });
 
@@ -197,8 +203,8 @@ export function item() {
     $(".filter-selector-button-text").text("由新到舊");
     data.sort = 1; // 1 表示由新到舊
 
-    scrollToTop();
     getData(data); // 重新获取并渲染数据
+    scrollToTop();
   });
 
   $(".old-to-new").click(function (e) {
@@ -207,7 +213,38 @@ export function item() {
     $(".filter-selector-button-text").text("由舊到新");
     data.sort = -1; // -1 表示由舊到新
 
-    scrollToTop();
     getData(data); // 重新获取并渲染数据
+    scrollToTop();
+  });
+
+  // 在 main.js 或 item.js 中的合適位置
+  document.addEventListener("click", (e) => {
+    e.preventDefault();
+    const target = e.target;
+
+    // 检查点击的元素是否是 card_share > fs-lg 中的链接
+    if (target.closest(".item-type")) {
+      const type = target.closest(".item-type").dataset.type; // 获取点击的 type 值
+
+      // 更新 filter-list 中的激活狀態
+      const filterItems = document.querySelectorAll(
+        ".filter-list .filter-item"
+      );
+      filterItems.forEach((item) => {
+        if (item.getAttribute("data-type") === type) {
+          item.classList.add("active");
+        } else {
+          item.classList.remove("active");
+        }
+      });
+
+      // 更新数据对象中的 type，并重新获取数据
+      data.type = type;
+      data.page = 1; // 重置到第一页
+
+      // 调用获取数据的函数，重新渲染卡片和分页
+      getData(data);
+      scrollToTop();
+    }
   });
 }
